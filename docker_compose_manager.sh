0f3a09fc784da8703499be8d7268ab46d804d418
Oracy Martos <ext.oracy.martos@farfetch.com>
2022-02-14 23:29:34 -0300

    Add metabse to manager

--- docker_compose_manager.sh
+++ docker_compose_manager.sh
@@ -6,8 +6,10 @@ if [ $action == "up" ];
 then
     echo "Turning data_source on" && docker-compose -f ./data_source/docker-compose.yaml up -d;
     echo "Turning airflow on" && docker-compose -f ./airflow/docker-compose.yaml up -d;
+    echo "Turning metabase on" && docker-compose -f ./metabase/docker-compose.yaml up -d;
 elif [ $action == "down" ];
 then
     echo "Turning airflow off" && docker-compose -f ./airflow/docker-compose.yaml down;
+    echo "Turning metabase off" && docker-compose -f ./metabase/docker-compose.yaml down;
     echo "Turning data_source off" && docker-compose -f ./data_source/docker-compose.yaml down;
 fi
