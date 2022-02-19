6a06ffb394e81b09ddcfe9ebf345a835ff1182bc
Oracy Martos <ext.oracy.martos@farfetch.com>
2022-02-18 23:25:16 -0300

    Add ip output to sh

--- docker_compose_manager.sh
+++ docker_compose_manager.sh
@@ -7,6 +7,10 @@ then
     echo "Turning data_source on" && docker-compose -f ./data_source/docker-compose.yaml up -d;
     echo "Turning airflow on" && docker-compose -f ./airflow/docker-compose.yaml up -d;
     echo "Turning metabase on" && docker-compose -f ./metabase/docker-compose.yaml up -d;
+    for OUTPUT in $(docker ps --format '{{.Names}}')
+    do
+        echo $OUTPUT":" $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $OUTPUT)
+    done
 elif [ $action == "down" ];
 then
     echo "Turning airflow off" && docker-compose -f ./airflow/docker-compose.yaml down;
